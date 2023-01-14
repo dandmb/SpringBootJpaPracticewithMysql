1bbeead68a155fae9c71f2f6b3656bf97ed06438
DAN BIZWA <bizwa_dan@BIZWAs-MacBook-Pro.local>
2023-01-14 19:31:31 +0100

    last version

--- src/test/java/com/dmb/SpringJpaCourse/entity/CourseTest.java
+++ src/test/java/com/dmb/SpringJpaCourse/entity/CourseTest.java
@@ -4,6 +4,9 @@ import com.dmb.SpringJpaCourse.repository.CourseRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
@@ -35,6 +38,76 @@ class CourseTest {
                 .build();
         courseRepository.save(course);
         //System.out.println("Courses : "+course);
+    }
+
+
+    @Test
+    public void findAllPagination(){
+        /*
+        Pageable firstPagewithThreeRecords= PageRequest.of(0,3);
+        Pageable secondPagewithTwoRecords= (Pageable) PageRequest.of(1,2);
+
+        List<Course> courses=courseRepository.findAll().getContent();
+
+        Long totalElements=courseRepository.findAll(firstPagewithThreeRecords).getTotalElements();
+        Long totalPages=courseRepository.findAll(firstPagewithThreeRecords).getTotalPages();
+
+
+        System.out.println("courses = "+totalElements);
+        System.out.println("courses = "+courses);
+
+        */
+    }
+
+    @Test
+    public void findAllSorting(){
+        /*
+        Pageable sortBytitle=
+                PageRequest.of(
+                        0,
+                        2,
+                        Sort.by("title")
+                );
+        Pageable sortByCreditDesc=
+                PageRequest.of(
+                        0,
+                        2,
+                        Sort.by("title").descending()
+                );
+
+       */
+    }
+
+    @Test
+    public void findByTitleContaining(){
+        Pageable firstPageTenRecords=PageRequest.of(0,10);
+        List<Course> courses=courseRepository.findByTitleContaining(
+                "P",
+                firstPageTenRecords
+        ).getContent();
+
+        System.out.println("courses = "+courses);
+    }
+
+    @Test
+    public void saveCourseWithStudentAndTeacher(){
+        Teacher teacher=Teacher.builder()
+                .firstName("Kool")
+                .lastName("Senior")
+                .build();
+        Student student=Student.builder()
+                .firstName("Papy")
+                .lastName("Test")
+                .emailId("papy@hm.com")
+                .build();
+
+        Course course=Course.builder()
+                .title("AI")
+                .credit(12)
+                .teacher(teacher)
+                .build();
+        course.addStudent(student);
+        courseRepository.save(course);
     }
 
 
